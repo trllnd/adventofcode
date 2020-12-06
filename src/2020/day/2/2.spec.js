e1ee55667ffafa9877f835ea002567bbff35c362
trllnd <18176331+trllnd@users.noreply.github.com>
2020-12-06 13:35:14 +0100

    day 2

---
+++ src/2020/day/2/2.spec.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+const input = require("./input.txt");
+
+describe("How many passwords are valid according to their policies?", () => {
+  it("should handle input", () => assert.strictEqual(response(input), 469));
+
+  ["1-3 a: abcde", "2-9 c: ccccccccc"].forEach((password) =>
+    it(`${password} should be valid`, () => {
+      assert(isValid(password));
+    })
+  );
+
+  ["1-3 b: cdefg"].forEach((password) =>
+    it(`${password} should not be valid`, () => {
+      assert(!isValid(password));
+    })
+  );
+});
+
+function response(passwords) {
+  let n = 0;
+  for (const password of passwords) n += isValid(password);
+  return n;
+}
+
+function isValid(password) {
+  const { lower, upper, char, value } = password.match(
+    /^(?<lower>\d+)-(?<upper>\d+)\s(?<char>.):\s(?<value>.+)/
+  ).groups;
+
+  const occurrence = value.split(char).length - 1;
+  return lower <= occurrence && occurrence <= upper;
+}
