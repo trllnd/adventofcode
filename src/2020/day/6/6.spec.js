58100e00b06589e1ce4510438773d8f2dc6e65c8
trllnd <18176331+trllnd@users.noreply.github.com>
2020-12-11 23:21:35 +0100

    day 6: part 2

--- src/2020/day/6/6.spec.js
+++ src/2020/day/6/6.spec.js
@@ -2,9 +2,25 @@ const assert = require("assert");
 const input = require("./input.txt").groups();
 
 describe("Day 6: Custom Customs", () => {
-  it("part 1: What is the sum of those counts?", () =>
-    assert.strictEqual(response(input), 6551));
+  it('part 1: For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?', () =>
+    assert.strictEqual(part1(input), 6551));
+
+  it('part 2: For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?', () =>
+    assert.strictEqual(part2(input), 3358));
 });
 
-const response = (input) =>
-  input.reduce((a, b) => a + new Set(b.join("")).size, 0);
+const part1 = (input) =>
+  input.reduce((a, group) => a + new Set(group.join("")).size, 0);
+
+const part2 = (input) =>
+  input.reduce(
+    (a, group) =>
+      a +
+      group
+        .flatMap((l) => l.split(""))
+        .sort()
+        .join("")
+        .match(/(.)\1*/g)
+        .filter((_) => _.length === group.length).length,
+    0
+  );
